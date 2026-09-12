dec24944dc1a4db14e9f7bc46348d1789a228397
vitorynoguti <113125880+yno09@users.noreply.github.com>
2026-09-12 20:08:10 -0300

    Add files via upload

---
+++ Relatorio 05/exercicio01CPP.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int combinar_equipes(int n)
+{
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else if (n > 1) {
+        return combinar_equipes(n-1) + combinar_equipes(n-2);
+    }
+}
+
+
+
+int main() 
+{
+    int n;
+
+    cout << "Digite o tamanho do chaveamento (n): ";
+    cin >> n;
+
+    cout << "Total de cenários de confrontos possíveis: ";
+    cout << combinar_equipes(n);
+    return 0;
+}
